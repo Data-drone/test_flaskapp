5f75f93c2fb02de6d1e211be3bde94d080ab702a
crazy449 <bpl.law@gmail.com>
2014-04-29 01:13:10 +0800

    table testing table code currently for csv and doesn't work for json

--- routes.py
+++ routes.py
@@ -6,6 +6,19 @@ db = SQLAlchemy(app)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://CarAccess:Cars_Data@localhost/CarsScraper'
 
+class TableData(db.Model):
+	__tablename__ = 'vw_LatestDataTable'
+	Manufacturer = db.Column(db.String(45))
+	Year = db.Column(db.DateTime)
+	Make = db.Column(db.String(100))
+	Model = db.Column(db.String(45))
+	Transmission = db.Column(db.String(45))
+	km = db.Column(db.Integer)
+	seller = db.Column(db.String(45))
+	Price = db.Column(db.Integer)
+	source = db.Column(db.String(128))
+	GuideID = db.Column(db.String(45), primary_key = True)	
+
 class Listing(db.Model):
 	__tablename__ = 'tbl_CarsRawData'
 	Manufacturer = db.Column(db.String(45))
@@ -21,6 +34,26 @@ class Listing(db.Model):
 	Price = db.Column(db.Integer)
 	source = db.Column(db.String(128))
 	timestamp = db.Column(db.DateTime)
+
+@app.route('/TblData/', methods=['GET'])
+def TblData():
+	if request.method == 'GET':
+		results = TableData.query.all()
+		
+		json_results = []
+		for result in results:
+			d = {'Manuf': result.Manufacturer,
+				 'Year': result.Year,
+				 'Make': result.Make,
+				 'Model': result.Model,
+				 'Transmission': result.Transmission,
+				 'km': result.km,
+				 'seller': result.seller,
+				 'Price': result.Price,
+				 'source': result.source}
+			json_results.append(d)
+
+	return jsonify(items=json_results)
 
 @app.route('/')
 def home():
